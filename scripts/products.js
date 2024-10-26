995e6734e404ddfb5246aebc68b4bf6f577aa44c
viccsn <victorsoaressn@gmail.com>
2024-10-26 20:06:54 -0300

    first attempt to fix the animation and files

--- scripts/products.js
+++ scripts/products.js
@@ -1,3 +1,10 @@
 function redirectToProduct(productName) {
     window.location.href = `info-products.html?product=${productName}`;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card) => {
+        card.classList.add('animate');
+    });
+});
